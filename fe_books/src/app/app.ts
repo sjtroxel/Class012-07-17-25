f56f99a6112f21b512ccb46c05c66d30aa24c4bf
sjtroxel <sjtroxel@protonmail.com>
2025-07-17 18:56:13 -0500

    eek pretty sure this isn't so right

--- fe_books/src/app/app.ts
+++ fe_books/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookListComponent } from './book-list/book-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, BookListComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
